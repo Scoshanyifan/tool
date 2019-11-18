aca46e44c5b1c72bace002403a89f56f7ba6a292
1 <2>
2019-11-18 17:29:50 +0800

    补充邮件注释

--- src/main/java/com/kunbu/common/util/mail/commonmail/CommonMailUtil.java
+++ src/main/java/com/kunbu/common/util/mail/commonmail/CommonMailUtil.java
@@ -1,9 +1,16 @@
 package com.kunbu.common.util.mail.commonmail;
 
 /**
- * @project: kunbutool
+ * http://commons.apache.org/proper/commons-email/userguide.html
+ *
+ * https://blog.csdn.net/decaizhang/article/details/7423030
+ *
+ *
  * @author: kunbu
  * @create: 2019-11-18 17:23
  **/
 public class CommonMailUtil {
+
+
+
 }
--- src/main/java/com/kunbu/common/util/mail/javamail/JavaMailUtil.java
+++ src/main/java/com/kunbu/common/util/mail/javamail/JavaMailUtil.java
@@ -1,7 +1,9 @@
 package com.kunbu.common.util.mail.javamail;
 
 /**
- * @project: kunbutool
+ *
+ *
+ *
  * @author: kunbu
  * @create: 2019-11-18 17:25
  **/
