4656021bbecffc79dbfa44de87d63c0797ad6ad0
1 <2>
2020-04-27 18:10:06 +0800

    修改log4j2的配置文件

--- src/main/java/com/kunbu/common/util/web/controller/ImgController.java
+++ src/main/java/com/kunbu/common/util/web/controller/ImgController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
 
 /**
  * @project: kunbutool
